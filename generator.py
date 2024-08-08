99c190247c5417136460b3f315e1a6d4eb888883
Jia Hng <jiahngkoh@gmail.com>
2024-08-08 21:47:37 +0800

    Fix no. filters bug in generator

--- generator.py
+++ generator.py
@@ -8,7 +8,34 @@ config.read('config.ini')
 
 IMG_RES = config.getint('params','IMG_RES')
 
-class Generator(nn.Module):
+class ResNetGenerator(nn.Module):
+    def __init__(self, n_resblocks):
+        super().__init__()
+
+        self.enc = nn.Sequential(
+            ConvInstanceNormRelu(64,kernel_size=(7,7),stride=1,padding="same"),
+            ConvInstanceNormRelu(128,kernel_size=(3,3),stride=2),
+            ConvInstanceNormRelu(256,kernel_size=(3,3),stride=2),
+        )
+        self.resblocks = nn.Sequential(
+            *(ResBlock(256,3) for _ in range(n_resblocks))
+        )
+        self.dec = nn.Sequential(
+            ConvTransposeInstanceNormRelu(128,kernel_size=(3,3),stride=2),
+            ConvTransposeInstanceNormRelu(64,kernel_size=(3,3),stride=2,output_padding=1),
+            nn.Conv2d(64, 3, kernel_size=(7,7), stride=1,padding="same"),
+            nn.Tanh()
+        )
+        self.model = nn.Sequential(
+            self.enc,
+            self.resblocks,
+            self.dec
+        )
+
+    def forward(self, X):
+        return self.model(X)
+
+class UNetGenerator(nn.Module):
     def __init__(self, n_resblocks):
         super().__init__()
 
@@ -34,9 +61,8 @@ class Generator(nn.Module):
 
     def forward(self, X):
         return self.model(X)
-
 if __name__ == '__main__':
-    gen = Generator(6)
+    gen = ResNetGenerator(6)
     inp = torch.zeros((1, 3, IMG_RES, IMG_RES))
     out = gen(inp)
     print(f"Output shape: {out.shape}")
