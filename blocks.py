c850295ce32a75b559d31a86676e2b842d18f65a
Jia Hng <jiahngkoh@gmail.com>
2024-08-12 11:27:23 +0800

    Fix InstanceNorm2d bug

--- blocks.py
+++ blocks.py
@@ -18,11 +18,11 @@ class ResBlock(nn.Module):
         return x + self.block(x)
 
 class ConvInstanceNormRelu(nn.Module):
-    def __init__(self, filters, leaky=False, norm=True,**kwargs):
+    def __init__(self, filters, leaky=True, norm=True,**kwargs):
         super().__init__()
         self.block = nn.Sequential(
-            nn.LazyConv2d(filters, bias=False,**kwargs),
-            *([nn.LazyInstanceNorm2d(filters, affine=True)] if norm else []),
+            nn.LazyConv2d(filters, bias=not norm,**kwargs),
+            *([nn.InstanceNorm2d(filters, affine=True)] if norm else []),
             nn.LeakyReLU(0.2) if leaky else nn.ReLU()
         )
     def forward(self, X):
@@ -33,59 +33,9 @@ class ConvTransposeInstanceNormRelu(nn.Module):
         super().__init__()
         self.block = nn.Sequential(
             nn.LazyConvTranspose2d(filters,bias=False, **kwargs),
-            nn.LazyInstanceNorm2d(filters, affine=True),
+            nn.InstanceNorm2d(filters, affine=True),
             *([nn.Dropout(0.5)] if dropout else []),
             nn.ReLU()
         )
     def forward(self, X):
         return self.block(X)
-
-
-class Upsampling(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size=4,
-        stride=2,
-        padding=1,
-        output_padding=0,
-        dropout=False,
-    ):
-        super().__init__()
-        self.block = nn.Sequential(
-            nn.ConvTranspose2d(in_channels, out_channels,
-                               kernel_size=kernel_size, stride=stride, 
-                               padding=padding, output_padding=output_padding, bias=False),
-            nn.InstanceNorm2d(out_channels, affine=True),
-        )
-        if dropout:
-            self.block.append(nn.Dropout(0.5))
-        self.block.append(nn.ReLU(True))
-        
-    def forward(self, x):
-        return self.block(x)
-    
-class Downsampling(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size=4,
-        stride=2,
-        padding=1,
-        norm=True,
-        lrelu=True,
-    ):
-        super().__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels,
-                      kernel_size=kernel_size, stride=stride, padding=padding, bias=not norm),
-        )
-        if norm:
-            self.block.append(nn.InstanceNorm2d(out_channels, affine=True))
-        if lrelu is not None:
-            self.block.append(nn.LeakyReLU(0.2, True) if lrelu else nn.ReLU(True))
-        
-    def forward(self, x):
-        return self.block(x)
--- discriminator.py
+++ discriminator.py
@@ -10,33 +10,19 @@ IMG_RES = config.getint('params','IMG_RES')
 BATCH_SIZE = config.getint('params','BATCH_SIZE')
 DISC_NOISE_STD = config.getfloat('params', 'DISC_NOISE_STD')
 
-# class Discriminator(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.model = torch.nn.Sequential(
-#             ConvInstanceNormRelu(64,kernel_size=(4,4),stride=2,padding=1,leaky=True,norm=False),
-#             ConvInstanceNormRelu(128,kernel_size=(4,4),stride=2,padding=1,leaky=True),
-#             ConvInstanceNormRelu(256,kernel_size=(4,4),stride=2,padding=1,leaky=True),
-#             ConvInstanceNormRelu(512,kernel_size=(4,4),stride=1,padding=1,leaky=True),
-#             nn.Conv2d(512,1,kernel_size=(4,4), stride=1, padding=1),
-#         )
-
-#     def forward(self, X):
-#         return self.model(X)
-
 class Discriminator(nn.Module):
-    def __init__(self, hid_channels=64, in_channels=3):
+    def __init__(self):
         super().__init__()
-        self.block = nn.Sequential(
-            Downsampling(in_channels, hid_channels, norm=False), # 64x128x128
-            Downsampling(hid_channels, hid_channels*2), # 128x64x64
-            Downsampling(hid_channels*2, hid_channels*4), # 256x32x32
-            Downsampling(hid_channels*4, hid_channels*8, stride=1), # 512x31x31
-            nn.Conv2d(hid_channels*8, 1, kernel_size=4, padding=1), # 1x30x30 (num_channels-h-w)
-        )# 1 channel for binary classification task, 30-30 spatial dimensions of the feature map
-        
-    def forward(self, x):
-        return self.block(x)
+        self.model = torch.nn.Sequential(
+            ConvInstanceNormRelu(64,kernel_size=(4,4),stride=2,padding=1,leaky=True,norm=False),
+            ConvInstanceNormRelu(128,kernel_size=(4,4),stride=2,padding=1,leaky=True),
+            ConvInstanceNormRelu(256,kernel_size=(4,4),stride=2,padding=1,leaky=True),
+            ConvInstanceNormRelu(512,kernel_size=(4,4),stride=1,padding=1,leaky=True),
+            nn.Conv2d(512,1,kernel_size=(4,4), stride=1, padding=1),
+        )
+
+    def forward(self, X):
+        return self.model(X)
 
 if __name__ == '__main__':
     disc = Discriminator()
